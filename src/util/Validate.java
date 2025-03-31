855171ee96b4b647149da788b9baa9bd845935c6
Pham Trung Hieu <ptrunghieu2507@gmail.com>
2025-03-31 16:16:29 +0700

    update

--- src/util/Validate.java
+++ src/util/Validate.java
@@ -4,78 +4,65 @@ import java.util.Scanner;
 
 public class Validate {
 
-   public static int validateInteger(Scanner sc) throws Exception {
-       try {
-           System.out.print("Nhập số nguyên: ");
-           if (sc.hasNextInt()) {
-               int n = sc.nextInt();
-               return n;
-           } else {
-               throw new Exception("Ko phải số nguyên");
-           }
-       } catch (Exception e) {
-          System.out.println(e.getMessage());
-       }
-       return 0;
-   }
+    public static int validateInteger(Scanner sc) throws Exception {
+        try {
+            System.out.print("Nhập số nguyên: ");
+            int n = sc.nextInt();
+            return n;
 
-   public static float validateFloat(Scanner sc) throws Exception {
-       try {
-           System.out.print("Nhập số thực float: ");
-           if (sc.hasNextFloat()) {
-               float n = sc.nextFloat();
-               return n;
-           } else {
-               throw new Exception("Ko phải số thực float");
-           }
-       } catch (Exception e) {
-           System.out.println(e.getMessage());
-       }
-       return 0;
-   }
+        } catch (Exception e) {
+            System.out.println("Ko đúng định dạng");
+        }
+        return 0;
+    }
 
-   public static double validateDouble(Scanner sc) throws Exception {
-       try {
-           System.out.print("Nhập số thực double: ");
-           if (sc.hasNextDouble()) {
-               double n = sc.nextDouble();
-               return n;
-           } else {
-               throw new Exception("Ko phải số thưc double");
-           }
-       } catch (Exception e){
-           System.out.println(e.getMessage());
-       }
-       return 0;
-   }
+    public static float validateFloat(Scanner sc) throws Exception {
+        try {
+            sc.nextLine();
+            System.out.print("Nhập số thực float: ");
+            float n = sc.nextFloat();
+            return n;
+        } catch (Exception e) {
+            System.out.println("Ko đúng định dạng");
+        }
+        return 0;
+    }
 
-   public static boolean validateBoolan(Scanner sc) throws Exception {
-       try {
-           System.out.print("Nhập true/false: ");
-           if (sc.hasNextBoolean()) {
-               boolean check = sc.nextBoolean();
-               return check;
-           } else {
-               throw new Exception("Ko phải kiểu boolean");
-           }
-       } catch (Exception e){
-           System.out.println(e.getMessage());
-       }
-       return false;
-   }
+    public static double validateDouble(Scanner sc) throws Exception {
+        try {
+            sc.nextLine();
+            System.out.print("Nhập số thực double: ");
 
-   public static String validateString(Scanner sc) throws Exception {
-       try {
-           System.out.print("nhập vào chuỗi: ");
-           String str = sc.nextLine();
-           if (str.isEmpty()) {
-               throw new Exception("Chuỗi không được để trống");
-           } else {
-               return str;
-           }
-       } catch (Exception e) {
-           System.out.println(e.getMessage());
-       }
-       return null;
-   }
+            double n = sc.nextDouble();
+            return n;
+
+        } catch (Exception e) {
+            System.out.println("Ko đúng định dạng");
+        }
+        return 0;
+    }
+
+    public static boolean validateBoolan(Scanner sc) throws Exception {
+        try {
+            sc.nextLine();
+            System.out.print("Nhập true/false: ");
+            boolean check = sc.nextBoolean();
+            return check;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public static String validateString(Scanner sc) throws Exception {
+        try {
+            sc.nextLine();
+            System.out.print("nhập vào chuỗi: ");
+            String str = sc.nextLine();
+            return str;
+        } catch (Exception e) {
+            System.out.println("Ko đúng định dạng");
+        }
+        return null;
+    }
 }
